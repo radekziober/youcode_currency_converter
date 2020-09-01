f3b959e2396d7c965bde72026ea9f78e61b035c3
Radek Ziober <kontakt@radekziober.pl>
2020-09-01 13:27:04 +0200

    delete if/else

--- js/script.js
+++ js/script.js
@@ -1,74 +1,71 @@
 {
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
+    let formElement = document.querySelector(".js-form");
+    let amountElement = document.querySelector(".js-amount");
 
-let fromCurrencyElement = document.querySelector(".js-fromCurrency");
-let toCurrencyElement = document.querySelector(".js-toCurrency");
+    let fromCurrencyElement = document.querySelector(".js-fromCurrency");
+    let toCurrencyElement = document.querySelector(".js-toCurrency");
 
-let convertButton = document.querySelector(".js-convertButton");
-let resultElement = document.querySelector(".js-result");
-
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    let convertButton = document.querySelector(".js-convertButton");
+    let resultElement = document.querySelector(".js-result");
 
 
-    let ratePLN = 1;
-    let rateEUR = 4.3969;
-    let rateUSD = 3.6924;
-    let rateCHF = 4.0884;
-
-    let amount = +amountElement.value;
-    let fromCurrency = fromCurrencyElement.value;
-    let toCurrency = toCurrencyElement.value;
-    let result;
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
 
-    switch (fromCurrency) {
-        case "PLN":
-            ratePLN = amount;
-            break;
+        let ratePLN = 1;
+        let rateEUR = 4.3969;
+        let rateUSD = 3.6924;
+        let rateCHF = 4.0884;
 
-        case "EUR":
-            ratePLN = amount * rateEUR;
-            break;
+        let amount = +amountElement.value;
+        let fromCurrency = fromCurrencyElement.value;
+        let toCurrency = toCurrencyElement.value;
+        let result;
 
-        case "USD":
-            ratePLN = amount * rateUSD;
-            break;
 
-        case "CHF":
-            ratePLN = amount * rateCHF;
-            break;
-    }
+        switch (fromCurrency) {
+            case "PLN":
+                ratePLN = amount;
+                break;
 
-    switch (toCurrency) {
-        case "PLN":
-            result = ratePLN;
-            break;
+            case "EUR":
+                ratePLN = amount * rateEUR;
+                break;
 
-        case "EUR":
-            result = ratePLN / rateEUR;
-            break;
+            case "USD":
+                ratePLN = amount * rateUSD;
+                break;
 
-        case "USD":
-            result = ratePLN / rateUSD;
-            break;
+            case "CHF":
+                ratePLN = amount * rateCHF;
+                break;
+        }
 
-        case "CHF":
-            result = ratePLN / rateCHF;
-            break;
+        switch (toCurrency) {
+            case "PLN":
+                result = ratePLN;
+                break;
 
-    }
+            case "EUR":
+                result = ratePLN / rateEUR;
+                break;
 
-    if (amount = 0) {
-        resultElement.innerText = `0.00 ${fromCurrency} = 0.00 ${toCurrency}`;
-    }
-    else {
+            case "USD":
+                result = ratePLN / rateUSD;
+                break;
+
+            case "CHF":
+                result = ratePLN / rateCHF;
+                break;
+
+        }
+
+
         resultElement.innerHTML = `<p>${amount.toFixed(2)} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}</p>`;
-    }
-   
 
-});
+
+
+    });
 
 }
